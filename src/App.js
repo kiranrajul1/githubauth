d9e51f6427c870ce3c9275b909012bcb75b6208d
kiranrajul1 <kiran@ini8labs.tech>
2024-03-29 11:49:24 +0530

    github frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {app} from './firebaseConfig';
+import {GithubAuthProvider,getAuth,signInWithPopup} from 'firebase/auth'
 function App() {
+  const githubProvider=new GithubAuthProvider();
+  const auth= getAuth();
+  const githubSignUp= () => {
+    signInWithPopup(auth,githubProvider)
+    .then((response) => {
+      console.log(response.user)
+    })
+    .catch((err) => {
+      console.log(err.code)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="auth-container">
+      <button onClick={githubSignUp}> Github</button>
     </div>
   );
 }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBIdx45uhs0dJhQswYgEBul0xsjJNzkQRA",
+  authDomain: "authentication-7f381.firebaseapp.com",
+  projectId: "authentication-7f381",
+  storageBucket: "authentication-7f381.appspot.com",
+  messagingSenderId: "941129187527",
+  appId: "1:941129187527:web:c998bbce457ed2ba867ee2",
+  measurementId: "G-CNPD9WRZTF"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
